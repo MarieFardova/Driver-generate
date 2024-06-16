9731fadba60a3595ab3e6719a0a152244e84462c
Marie77 <marie.fardova@post.cz>
2024-06-16 12:44:17 +0200

    excercise 1

--- src/main/java/com/course/selenium/TaskCheckConfig/BingSearch.java
+++ src/main/java/com/course/selenium/TaskCheckConfig/BingSearch.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -20,5 +22,8 @@ public class BingSearch {
         element.submit();
         driver.quit();
     }
+
+    
+
 
 }
