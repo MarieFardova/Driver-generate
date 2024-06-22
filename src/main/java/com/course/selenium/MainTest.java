dc7c2ee07be961482275329b6024b006bf3d0e3d
Marie77 <marie.fardova@post.cz>
2024-06-22 12:30:17 +0200

    Google search

---
+++ src/main/java/com/course/selenium/MainTest.java
@@ -0,0 +1,13 @@
+package com.course.selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test()
+    public void MainTest1() {
+        System.out.println("First JUnit Test...");
+        System.out.println("First JUnit Test...");
+    }
+}
